b83b775bff279850963493e7701a34edd7a4a24d
Abhilash A Savadatti <92322651+AbhilashSavadatti@users.noreply.github.com>
2024-09-13 12:06:39 +0000

    added components

--- crypto_trading/src/main.jsx
+++ crypto_trading/src/main.jsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './Redux/Store.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-       <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+     
     </BrowserRouter>
    
-  </StrictMode>,
+  </React.StrictMode>,
 )
